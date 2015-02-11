ec37ac037efeb65bc0d4d893823ffbc0b208d2d9
nimus0108 <nimus0108@gmail.com>
2015-02-10 23:31:58 -0500

    set up, playing around

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,12 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str : String = "Hello, playground"
+str = "Hello world"
+
+let modernProgrammingLanguage : String = "Swift"
+
+var greeting = str + modernProgrammingLanguage
+
+println("to make an app we use  + \(modernProgrammingLanguage)")
